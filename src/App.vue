afd4f7ab6fb340c4417d1326264401586b48c235
kelly <kellybackwards@gmail.com>
2021-11-18 16:15:17 -0500

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to This Vue.js App"/>
+  <Navbar />
+  <HelloWorld />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="masthead mb-auto">
+        <div class="inner">
+            <h3 class="masthead-brand">Kelly Lloyd</h3>
+            <nav class="nav nav-masthead justify-center">
+                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link" href="#">Resume</a>
+                <a class="nav-link" href="#">Projects</a>
+                <a class="nav-link" href="#">About</a>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
